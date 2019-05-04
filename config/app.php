cd6a4b463dd8c927a15294d53528206d0cc08b8c
Sakac934 <sakac934@msn.com>
2019-05-04 16:09:43 -0600

    second commit

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Ask Rachel'),
+    'name' => env('APP_NAME', 'Ask Rachel Belle'),
 
     /*
     |--------------------------------------------------------------------------
