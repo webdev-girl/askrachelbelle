287e6580c1c1812e865215bd3ed33bb8297e32d8
Sakac934 <sakac934@msn.com>
2019-05-06 18:33:59 -0600

    heoku css testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Ask Rachel belle</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="{{('css/belle.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::asset('css/belle.css')}}">
         <link rel="icon" type="image/x-icon" href="../images/flat-speech-bubble-with-question-marks.jpg"/favicon.png>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
